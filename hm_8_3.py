a01489ac9a0300e4ef489bc4d56373ca2909518c
YehorLitvinov <71943574+Weux94@users.noreply.github.com>
2025-01-19 12:22:47 +0200

    Add files via upload

---
+++ hm_8_3.py
@@ -0,0 +1,10 @@
+def find_unique_value(list):
+  unique_list = []
+  for item in list:
+      if item in unique_list:
+        continue
+      else:
+        unique_list.append(item)
+  return unique_list
+
+print(find_unique_value([1,2,3,4,5,5,2]))
