a3180626d05f2e42039248a779313ff915b8e990
YehorLitvinov <71943574+Weux94@users.noreply.github.com>
2025-01-19 12:31:26 +0200

    Update hm_8_3.py

--- hm_8_3.py
+++ hm_8_3.py
@@ -5,6 +5,6 @@ def find_unique_value(list):
         continue
       else:
         unique_list.append(item)
-  return unique_list
+  return unique_list[0]
 
 print(find_unique_value([1,2,3,4,5,5,2]))
